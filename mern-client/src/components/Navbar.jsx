bfaa93fc95b7fba2af2ca3769a0704972937c606
chamudi <chamuvithanage@gmail.com>
2024-07-05 10:37:40 +0530

    book-store app

---
+++ mern-client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+
+const Navbar = () => {
+    const [isMenuOpen,setMenuOpen]=useState(false);
+    const [isSticky,setSticky]=useState(false);
+
+    //toggle
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default Navbar
